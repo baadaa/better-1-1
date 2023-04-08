fa3426b19c7c21f014db39d943b2251c03bbeed9
Bumhan Yu <bumhan.yu@gmail.com>
2023-04-08 00:50:17 -0400

    persist random 3-pick for 24 hours unless shuffled by user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { CardSet } from '@/components/Card';
 import { Layout } from '@/components/Layout';
-import { pickRandomItems } from '@/utils/utils';
+import { pickRandomItems, setLocalStorage } from '@/utils/utils';
 import { Item } from '@/types';
 import { itemPool } from '@/data/items';
 
 export default function Home() {
   const [picks, setPicks] = useState<Array<Item>>([]);
 
-  const shuffleItems = () => setPicks(pickRandomItems(itemPool, 3));
+  const shuffleItems = () => {
+    const randomPicks = pickRandomItems(itemPool, 3);
+    setLocalStorage('b_1:1_picks', JSON.stringify(randomPicks));
+    setLocalStorage('b_1:1_timestamp', JSON.stringify(new Date().getTime()));
+    setPicks(randomPicks);
+  };
   useEffect(() => {
-    shuffleItems();
+    const savedItems = localStorage.getItem('b_1:1_picks');
+    const lastSaved = localStorage.getItem('b_1:1_timestamp');
+    const now = new Date().getTime();
+    const DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
+    if (!savedItems) {
+      shuffleItems();
+      return;
+    }
+    if (lastSaved && now - parseFloat(lastSaved) < DAY_IN_MILLISECONDS) {
+      setPicks(JSON.parse(savedItems));
+      return;
+    } else {
+      shuffleItems();
+    }
   }, []);
   return (
     <>
